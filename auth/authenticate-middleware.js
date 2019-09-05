3cb40823ea5738387a7fc604f1540f29e0c3baab
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-09-05 00:10:56 -0400

    fixed path for file and added jwt to middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,7 +1,21 @@
+const jwt = require('jsonwebtoken');
+const secret = require('../config/secrets');
+
+
+
 module.exports = (req, res, next) => {
-    if (req.session && req.session.user) {
-      next();
-    } else {
-      res.status(401).json({ you: "shall not pass!" });
-    }
-  };
+ const token = req.headers.authorization;
+
+ if (token) {
+   jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+     if (err) {
+       res.status(401).json({ message: 'NO! This ain’t it!' });
+     } else {
+       res.decodedJwt = decodedToken;
+       next();
+     }
+   });
+ } else {
+   res.status(401).json({ message: 'You do not pass!' });
+ }
+};
