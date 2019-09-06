cc5d6e37cf008f81cd10582906ed3d2de5bf5b44
Douglas Campbell <dacampbelltbone@gmail.com>
2019-09-05 23:22:29 -0400

    Tested /login and /register routes.

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -8,11 +8,91 @@ const server = require("../api/server");
 // structure of the return data ({field: value})
 
 describe("server.js", () => {
-  it("env should be set to testing", () => {
-    expect(process.env.DB_ENV).toBe("testing");
-  });
+  // it("env should be set to testing", () => {
+  //   expect(process.env.DB_ENV).toBe("testing");
+  // });
 
   afterAll(async () => {
     await db("users").truncate();
   });
+
+  describe("POST /register", () => {
+    it("should create user", () => {
+      return request(server)
+        .post("/api/auth/register")
+        .send({
+          username: "dangerousperson",
+          password: "eagleshark",
+          email: "dangerous@gmail.com"
+        })
+        .then(response => {
+          expect(response.status).toBe(201);
+          headers(response);
+        });
+    });
+
+    it("should return JSON object", () => {
+      return request(server)
+        .post("/api/auth/register")
+        .send({
+          username: "dangerousperson",
+          password: "eagleshark",
+          email: "dangerous@gmail.com"
+        })
+        .then(response => {
+          expect(response.type).toBe("application/json");
+        });
+    });
+
+    it("should return 500 status code for missing field", () => {
+      return request(server)
+        .post("/api/auth/register")
+        .send({
+          username: "dangerousperson",
+          password: "eagleshark",
+          email: ""
+        })
+        .then(response => {
+          expect(response.status).toBe(500);
+          expect(response.type).toBe("application/json");
+        });
+    });
+  });
+
+  describe("POST /login", () => {
+    it("should log user in", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .send({
+          username: "dangerousperson",
+          password: "eagleshark"
+        })
+        .then(response => {
+          headers(response);
+          expect(response.status).toBe(200);
+        });
+    });
+
+    it("should return a JSON object", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .send({ username: "dangerousperson", password: "eagleshark" })
+        .then(response => {
+          expect(response.type).toBe("application/json");
+        });
+    });
+
+    it("should return a 401 error for missing field", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .send({ username: "dangerousperson", password: "" })
+        .then(response => {
+          expect(response.status).toBe(401);
+        });
+    });
+  });
 });
+
+function headers(res) {
+  return expect(res.type).toBe("application/json");
+}
